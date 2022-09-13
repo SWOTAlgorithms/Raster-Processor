64b32b795e3af3a16c56ced07c147e74620bba41
Alexander T Corben <alexander.t.corben@jpl.nasa.gov>
2022-09-13 12:38:31 -0700

    Bug fix

--- src/SWOTRaster/raster_proc.py
+++ src/SWOTRaster/raster_proc.py
@@ -1257,33 +1257,29 @@ class RasterProcessor(object):
             else:
                 raise ValueError("Invalid Swath Side: {}".format(swath_side))
 
+            this_side_polygon_points = []
             for idx in idx_vec:
                 sc_llh = raster_crs.xyz2llh(sc_xyz[:,idx])
                 this_side_ll = raster_crs.terminal_loc_spherical(
                     sc_llh[0], sc_llh[1], this_side_crosstrack_dist,
-                    this_side_crosstrack_angle)
+                    this_side_crosstrack_angle[idx])
                 this_side_points_rad = [
                     [this_side_ll[0], this_side_ll[1], sc_llh[2]]]
 
                 if idx == 0 and alongtrack_start_buffer_dist is not None:
-                    sc_ll_buffer = raster_crs.terminal_loc_spherical(
-                        sc_llh[0], sc_llh[1], alongtrack_start_buffer_dist,
-                        np.deg2rad(np.mod(sc_velocity_heading[idx]-180, 360)))
                     this_side_ll_buffer = raster_crs.terminal_loc_spherical(
-                        sc_ll_buffer[0], sc_ll_buffer[1],
-                        this_side_crosstrack_dist, this_side_crosstrack_angle)
+                        this_side_ll[0], this_side_ll[1],
+                        alongtrack_start_buffer_dist,
+                        np.deg2rad(np.mod(sc_velocity_heading[idx]-180, 360)))
                     this_side_points_rad = \
                         [[this_side_ll_buffer[0], this_side_ll_buffer[1],
                           sc_llh[2]]] \
                         + this_side_points_rad
-
-                if idx == sc_xyz.shape[1]-1 and alongtrack_end_buffer_dist is not None:
-                    sc_ll_buffer = raster_crs.terminal_loc_spherical(
-                        sc_llh[0], sc_llh[1], alongtrack_end_buffer_dist,
-                        np.deg2rad(np.mod(sc_velocity_heading[idx], 360)))
+                elif idx == sc_xyz.shape[1]-1 and alongtrack_end_buffer_dist is not None:
                     this_side_ll_buffer = raster_crs.terminal_loc_spherical(
-                        sc_ll_buffer[0], sc_ll_buffer[1],
-                        this_side_crosstrack_dist, this_side_crosstrack_angle)
+                        this_side_ll[0], this_side_ll[1],
+                        alongtrack_start_buffer_dist,
+                        np.deg2rad(np.mod(sc_velocity_heading[idx], 360)))
                     this_side_points_rad = \
                         this_side_points_rad \
                         + [[this_side_ll_buffer[0], this_side_ll_buffer[1],
@@ -1296,7 +1292,9 @@ class RasterProcessor(object):
                     [point[:2]
                      for point in transf.TransformPoints(this_side_points_deg)]
 
-            polygon.extend(transf_this_side_points[::reverse_side])
+                this_side_polygon_points.extend(transf_this_side_points)
+
+            polygon.extend(this_side_polygon_points[::reverse_side])
 
         # if geodetic, swap lat and lon so that the e/w and n/s coords are
         # always in the same order regardless of projection
