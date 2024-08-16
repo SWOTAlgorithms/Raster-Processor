814c280b5e5f7c7698db9aa342bae45555aa0e91
Alexander T Corben <alexander.t.corben@jpl.nasa.gov>
2024-08-16 13:26:44 -0700

    Changed version number

--- src/SWOTRaster/products.py
+++ src/SWOTRaster/products.py
@@ -108,7 +108,7 @@ COMMON_ATTRIBUTES = odict([
      {'dtype': 'str' ,'value':'SWOT',
       'docstr': 'SWOT'}],
     ['references',
-     {'dtype': 'str', 'value': 'V1.2.1',
+     {'dtype': 'str', 'value': 'V1.3',
       'docstr': textjoin("""
           Published or web-based references that describe
           the data or methods used to product it. Provides version number of
@@ -174,7 +174,7 @@ COMMON_ATTRIBUTES = odict([
           Composite release identifier (CRID) of the data system used to
           generate this file""")}],
     ['product_version',
-     {'dtype': 'str', 'value': 'V1.2.1',
+     {'dtype': 'str', 'value': 'V1.3',
       'docstr': 'Version identifier of this data file'}],
     ['pge_name',
      {'dtype': 'str',
