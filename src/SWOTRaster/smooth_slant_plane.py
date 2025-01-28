3fd881ef25da39397f437e3b824e8eb499c0de0e
Alexander T Corben <alexander.t.corben@jpl.nasa.gov>
2025-01-27 16:52:09 -0800

    Convert time string to timestamp before sorting

--- src/SWOTRaster/smooth_slant_plane.py
+++ src/SWOTRaster/smooth_slant_plane.py
@@ -9,6 +9,7 @@ Author(s): Alexander Corben
 import logging
 import multiprocessing
 from functools import partial
+from datetime import datetime
 
 import bottleneck
 import numpy as np
@@ -186,8 +187,10 @@ def get_azimuth_offsets(scene_pixc, max_offset, tile_mask=None):
         'pixc_line_qual', 'not_in_tile'))
 
     # Sort tiles by granule start time
-    all_tiles_time_granule_start = \
-        scene_pixc['pixel_cloud']['tile_time_granule_start']
+    all_tiles_time_granule_start = np.array(
+        [strptime(time_granule_start, products.DATETIME_FORMAT_STR)
+         for time_granule_start
+         in scene_pixc['pixel_cloud']['tile_time_granule_start']])
     sort_idx = np.argsort(all_tiles_time_granule_start[tile_mask])
     tiles_idx = np.arange(len(all_tiles_time_granule_start))[tile_mask]
     outputs_idx = np.arange(len(tiles_idx))
