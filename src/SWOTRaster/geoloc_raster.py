Fixed unitless units set to 'l' instead of '1', fixed setting of swath extent attributes when adding pixcs, swath_side consistency pass

src/SWOTRaster/geoloc_raster.py
@@ -106,15 +106,14 @@ class GeolocRaster():
         max_chunk_size = self.algorithmic_config[
             'height_constrained_geoloc_max_chunk_size']
 
-        # Get the swath side (from tvp index, not nearest sensor index)
         line_idx = self.pixc['pixel_cloud']['pixc_line_index']
         tile_idx = self.pixc['pixel_cloud']['pixc_line_to_tile'][line_idx]
         tvp_idx = self.pixc['pixel_cloud'][
             'pixc_line_to_tvp'][line_idx].astype('i4')
-        swath_side = np.char.upper(self.pixc['tvp']['swath_side'][tvp_idx])
 
-        for side in ['L', 'R']:
-            side_mask = swath_side == side
+        for this_side in ['L', 'R']:
+            side_mask = np.char.upper(
+                self.pixc['tvp']['swath_side'][tvp_idx]) == this_side
             valid_mask = np.logical_not(np.logical_or.reduce((
                 np.ma.getmaskarray(self.pixc['pixel_cloud']['height']),
                 np.ma.getmaskarray(self.pixc['pixel_cloud']['latitude']),
@@ -170,7 +169,7 @@ class GeolocRaster():
             geoloc_fn = partial(
                 geoloc.pointcloud_height_geoloc_vect,
                 recompute_doppler=True, recompute_range=True, verbose=False,
-                max_iter_grad=1, height_goal=1.e-3, swath=side)
+                max_iter_grad=1, height_goal=1.e-3, swath=this_side)
             _geoloc_fn = partial(fn_star, geoloc_fn)
 
             if self.max_worker_processes > 1:

src/SWOTRaster/products.py
@@ -763,7 +763,7 @@ COMMON_VARIABLES = odict([
      odict([['dtype', 'u4'],
             ['long_name', 'number of water surface elevation pixels'],
             ['grid_mapping', 'crs'],
-            ['units', 'l'],
+            ['units', '1'],
             ['valid_min', 0],
             ['valid_max', 999999],
             ['coordinates', '[Raster coordinates]'],
@@ -775,7 +775,7 @@ COMMON_VARIABLES = odict([
      odict([['dtype', 'u4'],
             ['long_name', 'number of water surface area pixels'],
             ['grid_mapping', 'crs'],
-            ['units', 'l'],
+            ['units', '1'],
             ['valid_min', 0],
             ['valid_max', 999999],
             ['coordinates', '[Raster coordinates]'],
@@ -787,7 +787,7 @@ COMMON_VARIABLES = odict([
      odict([['dtype', 'u4'],
             ['long_name', 'number of sigma0 pixels'],
             ['grid_mapping', 'crs'],
-            ['units', 'l'],
+            ['units', '1'],
             ['valid_min', 0],
             ['valid_max', 999999],
             ['coordinates', '[Raster coordinates]'],
@@ -798,7 +798,7 @@ COMMON_VARIABLES = odict([
      odict([['dtype', 'u4'],
             ['long_name', 'number of other pixels'],
             ['grid_mapping', 'crs'],
-            ['units', 'l'],
+            ['units', '1'],
             ['valid_min', 0],
             ['valid_max', 999999],
             ['coordinates', '[Raster coordinates]'],
@@ -811,7 +811,7 @@ COMMON_VARIABLES = odict([
      odict([['dtype', 'f4'],
             ['long_name', 'fractional area of dark water'],
             ['grid_mapping', 'crs'],
-            ['units', 'l'],
+            ['units', '1'],
             ['valid_min', -1000],
             ['valid_max', 10000],
             ['coordinates', '[Raster coordinates]'],
@@ -1744,9 +1744,7 @@ class ScenePixc(Product):
         scene_pixc.time_coverage_start = pixc_tile.time_coverage_start
         scene_pixc.time_coverage_end = pixc_tile.time_coverage_end
 
-        swath_side = pixc_tile.swath_side
-
-        if swath_side.lower() == 'l':
+        if pixc_tile.swath_side.upper() == 'L':
             scene_pixc.left_first_longitude = pixc_tile.outer_first_longitude
             scene_pixc.left_last_longitude = pixc_tile.outer_last_longitude
             scene_pixc.left_first_latitude = pixc_tile.outer_first_latitude
@@ -1755,8 +1753,7 @@ class ScenePixc(Product):
             scene_pixc.right_last_longitude = pixc_tile.inner_last_longitude
             scene_pixc.right_first_latitude = pixc_tile.inner_first_latitude
             scene_pixc.right_last_latitude = pixc_tile.inner_last_latitude
-
-        elif swath_side.lower() == 'r':
+        else:
             scene_pixc.left_first_longitude = pixc_tile.inner_first_longitude
             scene_pixc.left_last_longitude = pixc_tile.inner_last_longitude
             scene_pixc.left_first_latitude = pixc_tile.inner_first_latitude
@@ -1916,7 +1913,7 @@ class ScenePixc(Product):
         tvp_swath_side = np.ma.concatenate(
             (self.tvp['swath_side'], other.tvp['swath_side']))
         [_, rev_idx] = np.unique(
-            np.column_stack((tvp_time, np.char.lower(tvp_swath_side) == 'r')),
+            np.column_stack((tvp_time, np.char.upper(tvp_swath_side) == 'R')),
             axis=0, return_inverse=True)
         unsorted_pixc_line_to_tvp = np.ma.concatenate((
             self.pixel_cloud['pixc_line_to_tvp'],
@@ -1951,11 +1948,11 @@ class ScenePixc(Product):
         def _strptime(d0, format_str=DATETIME_FORMAT_STR):
             return datetime.strptime(d0, format_str)
 
-        for swath_side in ['L', 'R']:
-            other_side_mask = np.char.lower(
-                other['pixel_cloud']['tile_swath_side']) == swath_side
-            self_side_mask = np.char.lower(
-                self['pixel_cloud']['tile_swath_side']) == swath_side
+        for this_side in ['L', 'R']:
+            other_side_mask = np.char.upper(
+                other['pixel_cloud']['tile_swath_side']) == this_side
+            self_side_mask = np.char.upper(
+                self['pixel_cloud']['tile_swath_side']) == this_side
             other_tile_granule_start_time = \
                 other['pixel_cloud']['tile_time_granule_start'][
                     other_side_mask]
@@ -1969,7 +1966,7 @@ class ScenePixc(Product):
                 self['pixel_cloud']['tile_time_granule_end'][
                     self_side_mask]
 
-            if swath_side.lower() == 'l':
+            if this_side == 'L':
                 start_lat_name = 'left_first_latitude'
                 start_lon_name = 'left_first_longitude'
                 end_lat_name = 'left_last_latitude'
@@ -1992,9 +1989,9 @@ class ScenePixc(Product):
             if np.any(other_side_mask) \
                and (not np.any(self_side_mask)
                     or datetime_str_comp(
-                        min(other_tile_granule_end_time, key=_strptime),
-                        min(self_tile_granule_end_time, key=_strptime),
-                        comp=op.lt)):
+                        max(other_tile_granule_end_time, key=_strptime),
+                        max(self_tile_granule_end_time, key=_strptime),
+                        comp=op.gt)):
                 setattr(klass, end_lat_name, getattr(other, end_lat_name))
                 setattr(klass, end_lon_name, getattr(other, end_lon_name))
 
@@ -2327,7 +2324,7 @@ class SceneTVP(Product):
         time = np.ma.concatenate((self.time, other.time))
         swath_side = np.ma.concatenate((self.swath_side, other.swath_side))
         [_, idx] = np.unique(
-            np.column_stack((time, np.char.lower(swath_side) == 'r')),
+            np.column_stack((time, np.char.upper(swath_side) == 'R')),
             axis=0, return_index=True)
         for key in klass.VARIABLES:
             setattr(klass, key, np.ma.concatenate((

src/SWOTRaster/smooth_slant_plane.py
@@ -102,8 +102,8 @@ def smooth_slant_plane(
     azimuth_offsets = np.zeros(
         scene_pixc['pixel_cloud']['tile_tile_name'].shape, dtype=int)
     for this_side in ['L', 'R']:
-        tile_side_mask = np.char.lower(
-            scene_pixc['pixel_cloud']['tile_swath_side']) == this_side.lower()
+        tile_side_mask = np.char.upper(
+            scene_pixc['pixel_cloud']['tile_swath_side']) == this_side
         range_offsets[tile_side_mask] = get_range_offsets(
             scene_pixc, tile_mask=tile_side_mask)
         azimuth_offsets[tile_side_mask] = get_azimuth_offsets(
@@ -284,7 +284,7 @@ def chunk_slant_map(var, az_idx, rng_idx, classif, classif_qual,
     LOGGER.info('Splitting slant plane into chunks')
     sort_idx = np.arange(len(var))
     for this_side in ['L', 'R']:
-        side_mask = np.char.lower(swath_side) == this_side.lower()
+        side_mask = np.char.upper(swath_side) == this_side
 
         # Skip if side_mask has no valid points
         if not np.any(side_mask):